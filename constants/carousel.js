7fda7b67bac9039610f94792c9f92e79374f8f81
Francisco Blanco <fran_boca@hotmail.com>
2022-01-17 13:15:00 -0300

    Adding Carousel && VideoPlayer

---
+++ constants/carousel.js
@@ -0,0 +1,15 @@
+import image1 from '../assets/carousel1.jpg';
+import image2 from '../assets/carousel2.jpg';
+import image3 from '../assets/carousel3.jpg';
+
+export const carousel = [
+    {
+        image: image1,
+    },
+    {
+        image: image2,
+    },
+    {
+        image: image3,
+    },
+]
--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -1,17 +1,54 @@
-import React from 'react'
-import { View, StyleSheet, ScrollView, Dimensions } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, ScrollView, Dimensions, Text, Image } from 'react-native'
+import { Video } from 'expo-av';
 import Covid from '../../components/Covid'
 import colors from '../../constants/colors'
+import Carousel from 'react-native-snap-carousel';
+import { carousel } from '../../constants/carousel'
 
-const width = Dimensions.get('window').width;
-const height = Dimensions.get('window').height;
+const { width, height } = Dimensions.get('window');
+
 
 export const HomeScreen = ({ navigation }) => {
+
+    const renderItem = ({ item }) => (
+        <View style={{
+            backgroundColor: 'yellow',
+            height: 200,
+            width: 200,
+        }}>
+            <Image
+                source={item.image}
+                resizeMode='contain'
+            />
+        </View>
+    )
+
     return (
         <>
             <ScrollView style={styles.container}>
-                <View  >
+                <View>
+
+                    <Carousel
+                        layout={"default"}
+                        firstItem={1}
+                        data={carousel}
+                        renderItem={renderItem}
+                        sliderWidth={width}
+                        itemWidth={200}
+                        itemHeight={240}
+                    />
+
                     <Covid navigation={navigation} />
+
+                    <Video
+                        source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+                        style={styles.video}
+                        useNativeControls
+                        resizeMode="cover"
+                        isLooping={false}
+                    />
+
                 </View>
             </ScrollView>
         </>
@@ -22,8 +59,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: colors.primary,
-        // justifyContent: 'center',
-        // alignItems: 'center',
     },
+    video: {
+        width: width,
+        height: height / 3,
+    }
 
 })
